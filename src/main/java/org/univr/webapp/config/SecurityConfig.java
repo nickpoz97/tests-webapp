3722ab933bda4022023767e404b6af85b075aee3
nickpoz97 <nega9705@gmail.com>
2022-08-05 01:17:22 +0200

    Login implemented

--- src/main/java/org/univr/webapp/config/SecurityConfig.java
+++ src/main/java/org/univr/webapp/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package org.univr.webapp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,11 +29,11 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
-    private final LoginService loginService;
+    private final DataSource dataSource;
 
     @Autowired
-    public SecurityConfig(LoginService loginService) {
-        this.loginService = loginService;
+    public SecurityConfig(LoginService loginService, @Qualifier("loginDataSource") DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     @Bean
@@ -41,11 +43,35 @@ public class SecurityConfig {
             //.authorizeRequests(requests -> requests.anyRequest().permitAll())
             .authorizeRequests(requests -> requests.anyRequest().authenticated())
             .httpBasic(withDefaults())
+                .formLogin(withDefaults())
             .build();
     }
 
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        return new InMemoryUserDetailsManager(loginService.getAllUsers());
+//    }
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        return new InMemoryUserDetailsManager(loginService.getAllUsers());
+    public UserDetailsManager users(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .usersByUsernameQuery("select username,password,enabled "
+                    + "from login "
+                    + "where username = ?")
+            .authoritiesByUsernameQuery("select username,ruolo "
+                    + "from login "
+                    + "where username = ?");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
--- src/main/java/org/univr/webapp/model/webappLogin/Login.java
+++ src/main/java/org/univr/webapp/model/webappLogin/Login.java
@@ -12,10 +12,13 @@ public class Login {
     @Column(name = "username")
     private String username;
 
-    @Column(name = "password", nullable = false, length = 64)
+    @Column(name = "password", nullable = false)
     private String password;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "ruolo", nullable = false)
     private Ruolo ruolo;
+
+    @Column(name = "enabled", nullable = false)
+    private Boolean enabled;
 }
