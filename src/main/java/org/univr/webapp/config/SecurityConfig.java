3fa82cc3d7834741bcbcce90fd7d5091c7b69cce
nickpoz97 <nega9705@gmail.com>
2022-08-05 00:34:09 +0200

    Working on security

--- src/main/java/org/univr/webapp/config/SecurityConfig.java
+++ src/main/java/org/univr/webapp/config/SecurityConfig.java
@@ -1,15 +1,25 @@
 package org.univr.webapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.univr.webapp.serviceLayer.webappLoginService.LoginService;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -17,25 +27,25 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+    private final LoginService loginService;
+
+    @Autowired
+    public SecurityConfig(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http
             .csrf(AbstractHttpConfigurer::disable)
-            .authorizeRequests(requests -> requests.anyRequest().permitAll())
-            //.authorizeRequests(requests -> requests.anyRequest().authenticated())
+            //.authorizeRequests(requests -> requests.anyRequest().permitAll())
+            .authorizeRequests(requests -> requests.anyRequest().authenticated())
             .httpBasic(withDefaults())
             .build();
     }
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService(){
-        UserDetails user =
-            User
-                .withDefaultPasswordEncoder()
-                .username("nick")
-                .password("psw")
-                .roles("INSEGNANTE")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+    public InMemoryUserDetailsManager userDetailsService() {
+        return new InMemoryUserDetailsManager(loginService.getAllUsers());
     }
 }
--- src/main/java/org/univr/webapp/serviceLayer/webappLoginService/LoginService.java
+++ src/main/java/org/univr/webapp/serviceLayer/webappLoginService/LoginService.java
@@ -1,18 +1,33 @@
 package org.univr.webapp.serviceLayer.webappLoginService;
 
 import com.google.common.hash.Hashing;
+import lombok.NoArgsConstructor;
 import lombok.extern.java.Log;
+import net.bytebuddy.build.Plugin;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.univr.webapp.dataLayer.webappLogin.LoginRepository;
 import org.univr.webapp.model.webappLogin.Login;
 import org.univr.webapp.model.webappLogin.Ruolo;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
-public class LoginService {
+public class LoginService { //implements UserDetailsService {
     private final LoginRepository loginRepository;
 
     @Autowired
@@ -27,5 +42,31 @@ public class LoginService {
                 .filter(login -> login.getPassword().equals(shasum));
 
         return result.map(Login::getRuolo).orElse(null);
+    }
+
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        Login user = loginRepository.findById(username)
+//            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+//
+//        return User
+//            .withUsername(user.getUsername())
+//            .password(user.getPassword())
+//            .authorities(user.getRuolo().name())
+//            .build();
+//    }
+
+    public List<UserDetails> getAllUsers(){
+        List<UserDetails> users = loginRepository.findAll().stream()
+            .map(
+                usr -> User
+                .withDefaultPasswordEncoder()
+                .username(usr.getUsername())
+                .password(usr.getPassword())
+                .roles(usr.getRuolo().toString())
+                .build())
+            .collect(Collectors.toList());
+
+        return users;
     }
 }
