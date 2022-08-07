fcfcc57bca54e5108afde7a1d3fbc31537c813fa
michele <bianchi.michele93@gmail.com>
2022-08-07 11:45:54 +0200

    aggiunta tabella test ecc

--- react-app/src/component/Test.js
+++ react-app/src/component/Test.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "../style.module.css";
 
 const TESTS_QUERY = `
     query {
@@ -11,21 +12,40 @@ const TESTS_QUERY = `
     }
     `;
 
+function formatDate(date){
+    var data =  date.split("-");
+    var newData = data[2] + "/" + data[1] + "/" + data[0];
+    return newData;
+}
+
 const Form = () => {
-    
+    const [tests, setTests] = React.useState([]);
     React.useEffect(() => {
         fetch('http://localhost:8080/graphql', {
-            mode:"no-cors",
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({query: TESTS_QUERY})
-        }).then(response => response.json()).then(data => console.log(data))
-    },[]);
-    
+        }).then(response => response.json())
+        .then(data => setTests(data.data.getAllTests))
+    },[]);    
     return(
         <div>
-            <h1>Ciao</h1>
+            <table className={styles.testTable}>
+                <tbody>
+                    <tr className={styles.testTr}>
+                        <th className={styles.testTh}>Nome</th>
+                        <th className={styles.testTh}>Data</th>
+                    </tr>
+                    {tests.map((test) => (
+                    <tr key={test.data + test.nome} className={styles.testTr}>
+                        <td className={styles.testTd}>{test.nome}</td>
+                        <td className={styles.testTd}> {formatDate(test.data)}</td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
 export default Form;
+
