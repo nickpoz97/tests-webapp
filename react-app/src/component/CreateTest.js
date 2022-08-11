e2e1767395113e222619f165f8828b8e329b1dad
michele <bianchi.michele93@gmail.com>
2022-08-11 13:35:41 +0200

    creazione test work in progress

--- react-app/src/component/CreateTest.js
+++ react-app/src/component/CreateTest.js
@@ -1,9 +1,9 @@
 import Appbar from "./Appbar";
-import { useLocation } from 'react-router-dom'
+import { useQuery } from 'react-query'
 import styles from "../style.module.css";
 import { useState } from 'react';
+
 import React from "react";
-import { style } from "@mui/system";
 
 const DOMANDE_QUERY = `
     query {
@@ -20,12 +20,26 @@ const DOMANDE_QUERY = `
       }
     }
     `;
-
-
+  
 function getOrario(){
   var today = new Date();
   var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
   return time;
+}
+
+function getToday(){
+  var date = new Date();
+  let d = new Date(date);
+  let month = (d.getMonth() + 1).toString();
+  let day = d.getDate().toString();
+  let year = d.getFullYear();
+  if (month.length < 2) {
+    month = '0' + month;
+  }
+  if (day.length < 2) {
+    day = '0' + day;
+  }
+  return [year, month, day].join('-');;
 }
 
 const CreateTest = () =>{
@@ -63,14 +77,72 @@ const CreateTest = () =>{
     }
   }
 
-  function deleteDomanda(e){
-    var array = [...this.state.arrayDomande]; // make a separate copy of the array
-    var index = array.indexOf(e.target.value)
-    if (index !== -1) {
-      array.splice(index, 1);
-      this.setState({arrayDomande: array});
-    }
+  function deleteDomanda(domanda){
+    var array = [...arrayDomande];
+    console.log(array);
+    var index = array.indexOf(domanda)
+    array.splice(index, 1);
+    setArrayDomande(array);
   }
+
+  function printButtonCheck(){
+    if(arrayDomande.length>0)
+      return(
+        <div>
+          <input id="ordineCasuale" name="ordineCasuale" type="checkbox"></input>
+          <label for="ordineCasuale">Ordine casuale</label>
+          <br></br>
+          <input id="domandeConNumero" name="domandeConNumero" type="checkbox"></input>
+          <label for="domandeConNumero">Domande con numero</label>
+          <br></br><br></br>
+          <button id="creaTest" className={styles.creaTestButton} onClick={creaTest}>Crea Test</button>
+        </div>
+      );
+  }
+
+  function creaTest(){
+    var bottone = document.getElementById("creaTest");
+    var nome_test = document.getElementById("nome_test");
+    var data = document.getElementById("data");
+    var ora = document.getElementById("ora");
+    var ordine_casuale = document.getElementById("ordineCasuale");
+    var domande_con_numero = document.getElementById("domandeConNumero");
+
+    var domande_string = "";
+    for(var i=0; i<arrayDomande.length; i++){
+      domande_string += '"'+arrayDomande[i].id+'",';
+    }
+
+    bottone.addEventListener("click", function() {
+      const ADD_TEST_QUERY = `
+        mutation{
+          addTest(
+              testInput: {
+                  giornoDelMese:`+ data.value.split("-")[2] +`,
+                  mese: `+ data.value.split("-")[2] +`,
+                  anno: `+ data.value.split("-")[0] +`,
+                  ora: `+ ora.value.split(":")[0] +`,
+                  minuto: `+ ora.value.split(":")[1] +`,
+                  nome:"`+ nome_test.value +`",
+                  ordineCasuale: `+ ordine_casuale.checked +`,
+                  domandeConNumero:  `+ domande_con_numero.checked +`,
+                  nomeDomande: [
+                    `+ domande_string +`
+                  ]
+              }
+          ){
+              success
+          }
+        }
+        `;
+        ExecuteQuery(ADD_TEST_QUERY)
+    })    
+  }
+
+  const ExecuteQuery = (testToAdd) =>{
+    console.log(testToAdd);
+  }
+
 
   const [domande, setDomande] = React.useState([]);
     React.useEffect(() => {
@@ -81,14 +153,16 @@ const CreateTest = () =>{
         }).then(response => response.json())
         .then(data => setDomande(data.data.getAllDomande))
     },[]); 
+
+    
   return(
     <div>
        <Appbar></Appbar>
        <div className={styles.divDomanda}>
         <h1>Creazione Test</h1>
-        Nome Test: <input type="text"/> <br></br><br></br>
-        Data: <input type="date"/>  <br></br><br></br>
-        Ora: <input type="text" placeholder={getOrario()}/>
+        Nome Test: <input id="nome_test" type="text"/> <br></br><br></br>
+        Data: <input id="data" type="date" value={getToday()}/>  <br></br><br></br>
+        Ora: <input id="ora" value={getOrario()}/>
        </div>
        <div className={styles.divDomanda}>
         <h2>Lista Domande</h2>
@@ -111,6 +185,7 @@ const CreateTest = () =>{
               <li className={styles.liCreateTest} key={domanda.nome} id={domanda.nome} value={domanda.testo}>{domanda.testo} <button className={styles.rimuoviCreateTest} onClick={() => { deleteDomanda(domanda) }}>Rimuovi</button></li>
             ))}
           </ol>
+          <div>{printButtonCheck()}</div>
         </div>
        </div>
     </div>
